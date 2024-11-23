d84034668a1b8958c26dcfcd71032d8d68c49b4c
Wallacy William dos Santos Oliveira <111467048+Wallacy0157@users.noreply.github.com>
2024-11-23 10:06:04 -0300

    Desenvolvimento do Servidor B

---
+++ src/sistemaDeBuscas/ServidorB.java
@@ -0,0 +1,40 @@
+package sistemaDeBuscas;
+
+import java.io.*;
+import java.net.*;
+
+public class ServidorB {
+    private static final int PORT_B = 6000;
+
+    public static void main(String[] args) throws IOException {
+        try (ServerSocket serverSocket = new ServerSocket(PORT_B)) {
+			System.out.println("Servidor B está rodando na porta " + PORT_B);
+
+			while (true) {
+			    Socket socket = serverSocket.accept();
+			    Thread requestHandler = new Thread(new Runnable() {
+			        public void run() {
+			            handleRequest(socket);
+			        }
+			    });
+			    requestHandler.start();
+			}
+		}
+    }
+
+    private static void handleRequest(Socket socket) {
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+             PrintWriter out = new PrintWriter(socket.getOutputStream(), true)) {
+
+            String busca = in.readLine();
+            System.out.println("Busca recebida do Servidor A: " + busca);
+            
+
+            //MECANISMO DE BUSCAS
+            //String resultado = //FUNÇÃO DE BUSCA
+            //out.println(resultado);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
